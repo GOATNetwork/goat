e2792ef08a11c07eb1fab19477b4d9a07e3e5bb3
ericlee <eric.l@metis.io>
2024-09-03 18:02:06 +0800

    minor tidy for GoatGuardHandler

--- app/ante.go
+++ app/ante.go
@@ -17,7 +17,6 @@ func NewAnteHandler(accKeeper ante.AccountKeeper, relayerKeeper goattypes.Relaye
 		ante.NewSetUpContextDecorator(), // outermost AnteDecorator. SetUpContext must be called first
 		GoatGuardHandler{relayerKeeper},
 		ante.NewValidateBasicDecorator(),
-		TxTimeoutHeightDecorator{},
 		ante.NewSetPubKeyDecorator(accKeeper),
 		ante.NewSigVerificationDecorator(accKeeper, signModeHandler),
 		ante.NewIncrementSequenceDecorator(accKeeper),
@@ -30,30 +29,36 @@ type GoatGuardHandler struct {
 	relayerKeeper goattypes.RelayerKeeper
 }
 
+type StdTx interface {
+	sdk.TxWithMemo
+	sdk.TxWithTimeoutHeight
+	authsigning.SigVerifiableTx
+}
+
 func (ante GoatGuardHandler) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate bool, next sdk.AnteHandler) (newCtx sdk.Context, err error) {
-	// memo length check
-	memoTx, ok := tx.(sdk.TxWithMemo)
+	stdTx, ok := tx.(StdTx)
 	if !ok {
 		return ctx, errorsmod.Wrap(sdkerrors.ErrTxDecode, "invalid transaction type")
 	}
 
-	if memoLength := len(memoTx.GetMemo()); memoLength > 0 {
+	if len(stdTx.GetMemo()) > 0 {
 		return ctx, errorsmod.Wrapf(sdkerrors.ErrMemoTooLarge, "no memo required")
 	}
 
-	// sig count check
-	sigTx, ok := tx.(authsigning.SigVerifiableTx)
-	if !ok {
-		return ctx, errorsmod.Wrap(sdkerrors.ErrTxDecode, "Tx must be a sigTx")
-	}
-
-	signers, err := sigTx.GetSigners()
+	signers, err := stdTx.GetSigners()
 	if err != nil {
 		return ctx, err
 	}
 
 	if len(signers) != 1 {
-		return ctx, errorsmod.Wrapf(sdkerrors.ErrTooManySignatures, "signer count %d > 1", len(signers))
+		return ctx, errorsmod.Wrapf(sdkerrors.ErrTooManySignatures, "signer count more than 1(%d)", len(signers))
+	}
+
+	timeoutHeight := stdTx.GetTimeoutHeight()
+	if timeoutHeight > 0 && uint64(ctx.BlockHeight()) > timeoutHeight {
+		return ctx, errorsmod.Wrapf(
+			sdkerrors.ErrTxTimeoutHeight, "block height: %d, timeout height: %d", ctx.BlockHeight(), timeoutHeight,
+		)
 	}
 
 	// msg allow list check
@@ -80,17 +85,21 @@ func (ante GoatGuardHandler) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate boo
 	if !simulate {
 		// the message should be belong to goad namespace
 		for _, msg := range msgs {
-			msgName := msg.ProtoReflect().Descriptor().FullName()
+			msgName := string(msg.ProtoReflect().Descriptor().FullName())
 			switch ctx.ExecMode() {
 			case sdk.ExecModeCheck, sdk.ExecModeReCheck, sdk.ExecModePrepareProposal: // only accept relayer txs in the mempool
-				if err := relayerTxOnly(string(msgName)); err != nil {
+				if err := relayerTxOnly(msgName); err != nil {
 					return ctx, err
 				}
 			case sdk.ExecModeProcessProposal, sdk.ExecModeFinalize:
-				if string(msgName) == "goat.goat.v1.MsgNewEthBlock" {
+				if (msgName) == "goat.goat.v1.MsgNewEthBlock" {
+					if timeoutHeight != uint64(ctx.BlockHeight()) {
+						return ctx, errorsmod.Wrapf(
+							sdkerrors.ErrTxTimeoutHeight, "MsgNewEthBlock timeout height should be current block: %d", ctx.BlockHeight())
+					}
 					continue
 				}
-				if err := relayerTxOnly(string(msgName)); err != nil {
+				if err := relayerTxOnly(msgName); err != nil {
 					return ctx, err
 				}
 			}
@@ -98,32 +107,4 @@ func (ante GoatGuardHandler) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate boo
 	}
 
 	return next(newCtx, tx, simulate)
-}
-
-type TxTimeoutHeightDecorator struct{}
-
-func (txh TxTimeoutHeightDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate bool, next sdk.AnteHandler) (sdk.Context, error) {
-	timeoutTx, ok := tx.(sdk.TxWithTimeoutHeight)
-	if !ok {
-		return ctx, errorsmod.Wrap(sdkerrors.ErrTxDecode, "expected tx to implement TxWithTimeoutHeight")
-	}
-
-	timeoutHeight := timeoutTx.GetTimeoutHeight()
-
-	if msgs := tx.GetMsgs(); len(msgs) == 1 {
-		if _, ok := msgs[0].(*goattypes.MsgNewEthBlock); ok {
-			if timeoutHeight != uint64(ctx.BlockHeight()) {
-				return ctx, errorsmod.Wrapf(
-					sdkerrors.ErrTxTimeoutHeight, "MsgNewEthBlock timeout height should be current block: %d", ctx.BlockHeight())
-			}
-		}
-	}
-
-	if timeoutHeight > 0 && uint64(ctx.BlockHeight()) > timeoutHeight {
-		return ctx, errorsmod.Wrapf(
-			sdkerrors.ErrTxTimeoutHeight, "block height: %d, timeout height: %d", ctx.BlockHeight(), timeoutHeight,
-		)
-	}
-
-	return next(ctx, tx, simulate)
 }
