5d5100dbd37c5bf1b4664ec61383f325726d5acd
ericlee <eric.l@metis.io>
2024-09-16 22:02:57 +0800

    feat(goat): diable NewEthBlock cli

--- x/goat/module/autocli.go
+++ x/goat/module/autocli.go
@@ -11,8 +11,12 @@ func (am AppModule) AutoCLIOptions() *autocliv1.ModuleOptions {
 	return &autocliv1.ModuleOptions{
 		Query: &autocliv1.ServiceCommandDescriptor{
 			Service:           modulev1.Query_ServiceDesc.ServiceName,
+			RpcCommandOptions: []*autocliv1.RpcCommandOptions{},
+		},
+		Tx: &autocliv1.ServiceCommandDescriptor{
+			Service: modulev1.Msg_ServiceDesc.ServiceName,
 			RpcCommandOptions: []*autocliv1.RpcCommandOptions{
-				// this line is used by ignite scaffolding # autocli/query
+				{RpcMethod: "NewEthBlock", Skip: true},
 			},
 		},
 	}
