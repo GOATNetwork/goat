d7d5858468f762ee587df17ed8f2383149f0019a
ericlee <eric.l@metis.io>
2025-01-24 23:01:29 +0800

    feat(bitcoin): update the hardfork timestamp

--- x/bitcoin/types/hardfork.go
+++ x/bitcoin/types/hardfork.go
@@ -11,6 +11,6 @@ func (h *Hardfork) IsWithdrawalV2Enable(time time.Time) bool {
 }
 
 var Hardforks = map[string]*Hardfork{
-	"goat-testnet3": {WithdrawalV2: 1600000000}, // todo
-	"goat-mainnet":  {WithdrawalV2: 1600000000}, // todo
+	"goat-testnet3": {WithdrawalV2: 1735099200}, // 2024-12-25T04:00:00Z
+	"goat-mainnet":  {WithdrawalV2: 1735135200}, // 2024-12-25T14:00:00Z
 }
