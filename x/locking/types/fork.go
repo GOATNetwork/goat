4cdb5bcbdca584f2e7e3e3cc8c794f5799823de0
ericlee <eric.l@metis.io>
2025-12-11 23:24:51 +0800

    fix(locking): update fork height for testnet3

--- x/locking/types/fork.go
+++ x/locking/types/fork.go
@@ -4,5 +4,5 @@ var OsakaForkHeight = map[string]int64{
 	"unitest": 10, // it's for unit test
 
 	"goat-mainnet":  8821000, // estimate at 2025-12-19 16:00:00 UTC
-	"goat-testnet3": 9440000, // estimate at 2025-12-15 15:00:00 UTC
+	"goat-testnet3": 9695800, // estimate at 2025-12-15 15:00:00 UTC
 }
