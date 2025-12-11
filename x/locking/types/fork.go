bfe136c57669e2fbc404d94ba792c8f1455836f0
ericlee <eric.l@metis.io>
2025-12-11 21:56:24 +0800

    update OsakaForkHeight with correct mainnet and testnet3 fork heights

--- x/locking/types/fork.go
+++ x/locking/types/fork.go
@@ -1,10 +1,8 @@
 package types
 
-import "math"
-
 var OsakaForkHeight = map[string]int64{
 	"unitest": 10, // it's for unit test
 
-	"mainnet":  math.MaxInt64, // TODO: set the mainnet fork height
-	"testnet3": math.MaxInt64, // TODO: set the testnet3 fork height
+	"mainnet":  8821000, // estimate at 2025-12-19 16:00:00 UTC
+	"testnet3": 9440000, // estimate at 2025-12-15 15:00:00 UTC
 }
