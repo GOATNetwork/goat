41d90e05a1186aa0afd7037ebbff6f87d5e30b34
ericlee <eric.l@metis.io>
2025-12-11 22:17:32 +0800

    fix(locking): correct chain id for OsakaForkHeight

--- x/locking/types/fork.go
+++ x/locking/types/fork.go
@@ -3,6 +3,6 @@ package types
 var OsakaForkHeight = map[string]int64{
 	"unitest": 10, // it's for unit test
 
-	"mainnet":  8821000, // estimate at 2025-12-19 16:00:00 UTC
-	"testnet3": 9440000, // estimate at 2025-12-15 15:00:00 UTC
+	"goat-mainnet":  8821000, // estimate at 2025-12-19 16:00:00 UTC
+	"goat-testnet3": 9440000, // estimate at 2025-12-15 15:00:00 UTC
 }
